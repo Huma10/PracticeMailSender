dbc7728ac4092ba8ae84232e6d06b7d9a5024093
Huma10 <khanhuma1021994@gmail.com>
2022-08-20 15:27:00 +0530

    Second Commit

--- src/main/java/com/mail/bean/Employee.java
+++ src/main/java/com/mail/bean/Employee.java
@@ -3,11 +3,28 @@ package com.mail.bean;
 import lombok.Getter;
 import lombok.Setter;
 
-@Setter
-@Getter
+
 public class Employee {
 
 	private String email;
 	
 	private String name;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 }
